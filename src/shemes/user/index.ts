f76215fe56c6e9cfc4056b87042e53f54e7392bf
POZ <antoivans@gmail.com>
2022-10-18 21:45:00 +0300

    Feat: add swagger + improve error handling

--- src/shemes/user/index.ts
+++ src/shemes/user/index.ts
@@ -1,11 +1,16 @@
 import Joi from 'joi';
 
-import { CreateUserPayload } from './interfaces';
+import { CreateUserPayload, LoginUserPayload } from './interfaces';
 
 const usernamePattern = /^[A-Za-z]\w{2,29}$/;
 
 export const createUserSchema = Joi.object<CreateUserPayload>({
   email: Joi.string().email().required(),
   name: Joi.string().pattern(usernamePattern, 'username'),
+  password: Joi.string().required(),
+});
+
+export const loginUserSchema = Joi.object<LoginUserPayload>({
+  email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
